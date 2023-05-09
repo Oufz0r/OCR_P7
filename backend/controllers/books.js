c61608523d8fc663ba97c42963251b6b64282052
Damien PERNIN <daz.oufzor@gmail.com>
2023-05-09 10:50:57 +0200

    retouches try

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -98,29 +98,37 @@ exports.modifyBook = (req, res, next) => {
 
 
 exports.deleteBook = (req, res, next) => {
-    Book.findOne({ _id: req.params.id})
-    .then(book => {
-        if (book.userId != req.auth.userId) {
-            res.status(403).json({message: 'Non-autorisé'});
-        } else {
-            const filename = book.imageUrl.split('/images/')[1];
-            fs.unlink(`images/${filename}`, () => {
-                Book.deleteOne({_id: req.params.id})
-                .then(() => { res.status(200).json({message: 'Livre supprimé !'})})
-                .catch(error => res.status(401).json({ error }));
-            });
-        }
-    })
-    .catch( error => {
-        res.status(500).json({ error });
-    });
+    try {
+        Book.findOne({ _id: req.params.id})
+        .then(book => {
+            if (book.userId != req.auth.userId) {
+                res.status(403).json({message: 'Non-autorisé'});
+            } else {
+                const filename = book.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Book.deleteOne({_id: req.params.id})
+                    .then(() => { res.status(200).json({message: 'Livre supprimé !'})})
+                    .catch(error => res.status(401).json({ error }));
+                });
+            }
+        })
+        .catch( error => {
+            res.status(500).json({ error });
+        });
+    } catch (error) {
+        res.status(400).json({ error });
+    }
 };
 
 
 exports.getOneBook = (req, res, next) => {
-    Book.findOne({ _id: req.params.id })
-    .then(book => res.status(200).json( book ))
-    .catch(error => res.status(404).json({ error }));
+    try {
+        Book.findOne({ _id: req.params.id })
+        .then(book => res.status(200).json( book ))
+        .catch(error => res.status(404).json({ error }));
+    } catch (error) {
+        res.status(400).json({ error });
+    }
 };
 
 
@@ -139,49 +147,57 @@ exports.bestRatings = async (req, res, next) => {
 
 
 exports.getAllBook = (req, res, next) => {
-    Book.find()
-    .then(books => res.status(200).json( books ))
-    .catch(error => res.status(400).json({ error }));
+    try {
+        Book.find()
+        .then(books => res.status(200).json( books ))
+        .catch(error => res.status(400).json({ error }));
+    } catch (error) {
+        res.status(400).json({ error });
+    }
 };
 
 
 exports.rateBook = (req, res, next) => {
-    // On récupère le livre à noter
-    Book.findOne({ _id: req.params.id })
-    .then((book) => {
-        const ratings = book.ratings;
-        const userId = req.auth.userId;
-        // On vérifie que l'utilisateur n'a pas déjà noté ce livre
-        const userRating = ratings.find(rating => rating.userId === userId);
+    try {
+        // On récupère le livre à noter
+        Book.findOne({ _id: req.params.id })
+        .then((book) => {
+            const ratings = book.ratings;
+            const userId = req.auth.userId;
+            // On vérifie que l'utilisateur n'a pas déjà noté ce livre
+            const userRating = ratings.find(rating => rating.userId === userId);
 
-        if (userRating) {
-            // return res.status(400).json({ error: 'Vous avez déjà noté ce livre' });
-            return res.status(400).json({ error });
-        }
+            if (userRating) {
+                // return res.status(400).json({ error: 'Vous avez déjà noté ce livre' });
+                return res.status(400).json({ error });
+            }
 
-        // On calcule la somme des notes présentes
-        const sum = ratings.reduce((acc, rating) => acc + rating.grade, 0);
-        const newRating = {
-            userId,
-            grade: req.body.rating
-        }
-        // On push la nouvelle note temporairement pour faire la moyenne
-        ratings.push(newRating);
+            // On calcule la somme des notes présentes
+            const sum = ratings.reduce((acc, rating) => acc + rating.grade, 0);
+            const newRating = {
+                userId,
+                grade: req.body.rating
+            }
+            // On push la nouvelle note temporairement pour faire la moyenne
+            ratings.push(newRating);
 
-        // On fait la moyenne en ajoutant la nouvelle note
-        const average = (sum + newRating.grade) / ratings.length;
-        // On arrondit à une decimale
-        const roundedAverage = parseFloat(average.toFixed(1));
+            // On fait la moyenne en ajoutant la nouvelle note
+            const average = (sum + newRating.grade) / ratings.length;
+            // On arrondit à une decimale
+            const roundedAverage = parseFloat(average.toFixed(1));
 
-        // On met à jour le livre en base de données
-        Book.findOneAndUpdate(
-            { _id: req.params.id },
-            { $push: { ratings: newRating }, averageRating: roundedAverage },
-            { new: true, returnOriginal: false }
-        )
-            // On transmet le livre mis à jour au front via la réponse
-            .then((updatedBook) => res.status(200).json( updatedBook ))
-            .catch(error => res.status(401).json({ error }));
-    })
-    .catch(error => res.status(400).json({ error }));
+            // On met à jour le livre en base de données
+            Book.findOneAndUpdate(
+                { _id: req.params.id },
+                { $push: { ratings: newRating }, averageRating: roundedAverage },
+                { new: true, returnOriginal: false }
+            )
+                // On transmet le livre mis à jour au front via la réponse
+                .then((updatedBook) => res.status(200).json( updatedBook ))
+                .catch(error => res.status(401).json({ error }));
+        })
+        .catch(error => res.status(400).json({ error }));
+    } catch (error) {
+        res.status(400).json({ error });
+    }
 };
